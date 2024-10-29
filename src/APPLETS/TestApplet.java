f46e595d959c5b55a0cc1518c9e83911e19f6f0d
Navpreet0981 <navi2005saini@gmail.com>
2024-10-30 00:37:43 +0530

    applets

---
+++ src/APPLETS/TestApplet.java
@@ -0,0 +1,15 @@
+package APPLETS;
+import java.applet.*;
+import java.awt.*;
+
+//<applet code ="TestApplet" width=200 height=60></applet>
+public class TestApplet {
+    public void paint(Graphics w){
+        w.drawString("Welcome To Applet",30,30);
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
