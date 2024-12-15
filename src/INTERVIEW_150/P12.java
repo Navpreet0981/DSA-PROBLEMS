ed943d7cfa49c03cb37313111757b4041a8e526f
Navpreet0981 <navi2005saini@gmail.com>
2024-12-15 08:29:13 +0530

    Interview 11,12

--- src/INTERVIEW_150/P12.java
+++ src/INTERVIEW_150/P12.java
@@ -3,6 +3,7 @@ package INTERVIEW_150;
 import java.util.*;
 
 public class P12 {
+//    Implement different operation on list
     private Map<Integer, Integer> map;
     private List<Integer> list;
     private Random random;
@@ -44,4 +45,24 @@ public class P12 {
         int randomIndex = random.nextInt(list.size());
         return list.get(randomIndex);
     }
+
+    /*
+    Example 1:
+
+Input
+["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+[[], [1], [2], [2], [], [1], [2], []]
+Output
+[null, true, false, true, 2, true, false, 2]
+
+Explanation
+RandomizedSet randomizedSet = new RandomizedSet();
+randomizedSet.insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomizedSet.remove(2); // Returns false as 2 does not exist in the set.
+randomizedSet.insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
+randomizedSet.getRandom(); // getRandom() should return either 1 or 2 randomly.
+randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contains [2].
+randomizedSet.insert(2); // 2 was already in the set, so return false.
+randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
+     */
 }
